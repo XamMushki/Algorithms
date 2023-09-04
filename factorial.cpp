5e7062b97bb4b953f01e50e7c775f7dba909b930
XamMushki <xahidmushki13@gmail.com>
2023-09-05 01:16:40 +0530

    Factorial - recursive and iterative

--- factorial.cpp
+++ factorial.cpp
@@ -12,7 +12,7 @@ int factorialRecursive(int num){
     if(num<=1){
         return 1;
     }
-    return num * factorial(num-1);
+    return num * factorialRecursive(num-1);
 }
 int main() {
     int num;
